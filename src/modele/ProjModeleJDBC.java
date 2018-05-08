a0eb8dd6b03890ce1a13d5e293c5eee9b27168d5
Rikako <rikako@hotmail.fr>
2018-05-08 12:34:48 +0200

    Ajout du modèle JDBC

---
+++ src/modele/ProjModeleJDBC.java
@@ -0,0 +1,201 @@
+package modele;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import connections.DBconnect;
+
+/**
+ *
+ * @author Tiffany De Schinckel
+ */
+public class ProjModeleJDBC extends ProjModele {
+
+    Connection dbconnect;
+
+    public ProjModeleJDBC() {
+        dbconnect = DBconnect.getConnection();
+        if (dbconnect == null) {
+            System.err.println("erreur de connexion => arrêt du pgm");
+            System.exit(1);
+        }
+    }
+
+    public void close() {
+        try {
+            dbconnect.close();
+        } catch (Exception e) {
+            System.err.println("erreur lors de la fermeture de la connexion " + e);
+        }
+    }
+
+    @Override
+    public void populate() {
+        //ne rien faire car données déjà présentes dans DB
+    }
+
+    @Override
+    public String ajouterObjet(Object o) {
+        String msg = "";
+        String query;
+
+        //On vérifie que l'objet n'est pas null
+        if (o == null) {
+            return "Objet null. Ajout impossible";
+        }
+
+        //On détermine la table correspondante au type d'objet
+        if (o instanceof Projet) {
+            query = "insert into PROJ_PROJET(TITRE_PROJ,CLIENT_PROJ,DATE_DEBUT,DATE_BUTOIR) VALUES (?,?,?,?)";
+            PreparedStatement pstm = null;
+
+            try {
+                pstm = dbconnect.prepareStatement(query);
+                pstm.setString(1, ((Projet) o).getTitre());
+                //TODO Récupérer l'id du client dans la BDD
+                //TODO Récupérer les dates en format Date
+                int n = pstm.executeUpdate();
+
+                if (n == 1) {
+                    msg = "Ajout projet effectué";
+                } else {
+                    msg = "Ajout projet non effectué";
+                }
+            } catch (Exception e) {
+                msg = "Erreur lors de l'ajout du projet " + e;
+            } finally {
+                try {
+                    if (pstm != null) {
+                        pstm.close();
+                    }
+                } catch (Exception e) {
+                    System.out.println("erreur de fermeture du preparedStatement " + e);
+                }
+
+            }
+
+        }
+
+        if (o instanceof Employe) {
+            query = "insert into PROJ_EMPLOYE(NOM_EMP, PRENOM_EMP, GSM_EMP, EMAIL_EMP) VALUES (?,?,?,?)";
+            PreparedStatement pstm = null;
+
+            try {
+                pstm = dbconnect.prepareStatement(query);
+                pstm.setString(1, ((Employe) o).getNom());
+                pstm.setString(2, ((Employe) o).getPrenom());
+                pstm.setString(3, ((Employe) o).getGsm());
+                pstm.setString(4, ((Employe) o).getEmail());
+                int n = pstm.executeUpdate();
+
+                if (n == 1) {
+                    msg = "Ajout employé effectué";
+                } else {
+                    msg = "Ajout employé non effectué";
+                }
+            } catch (Exception e) {
+                msg = "Erreur lors de l'ajout de l'employé " + e;
+            } finally {
+                try {
+                    if (pstm != null) {
+                        pstm.close();
+                    }
+                } catch (Exception e) {
+                    System.out.println("erreur de fermeture du preparedStatement " + e);
+                }
+
+            }
+        }
+
+        if (o instanceof Client) {
+            query = "insert into PROJ_() VALUES ()";
+            PreparedStatement pstm = null;
+
+            try {
+                pstm = dbconnect.prepareStatement(query);
+
+                int n = pstm.executeUpdate();
+
+                if (n == 1) {
+                    msg = "Ajout projet effectué";
+                } else {
+                    msg = "Ajout projet non effectué";
+                }
+            } catch (Exception e) {
+                msg = "Erreur lors de l'ajout du projet " + e;
+            } finally {
+                try {
+                    if (pstm != null) {
+                        pstm.close();
+                    }
+                } catch (Exception e) {
+                    System.out.println("erreur de fermeture du preparedStatement " + e);
+                }
+
+            }
+        }
+
+        if (o instanceof Discipline) {
+            query = "insert into PROJ_() VALUES ()";
+            PreparedStatement pstm = null;
+
+            try {
+                pstm = dbconnect.prepareStatement(query);
+
+                int n = pstm.executeUpdate();
+
+                if (n == 1) {
+                    msg = "Ajout projet effectué";
+                } else {
+                    msg = "Ajout projet non effectué";
+                }
+            } catch (Exception e) {
+                msg = "Erreur lors de l'ajout du projet " + e;
+            } finally {
+                try {
+                    if (pstm != null) {
+                        pstm.close();
+                    }
+                } catch (Exception e) {
+                    System.out.println("erreur de fermeture du preparedStatement " + e);
+                }
+
+            }
+        }
+
+        if (o instanceof Travail) {
+            query = "insert into PROJ_() VALUES ()";
+            PreparedStatement pstm = null;
+
+            try {
+                pstm = dbconnect.prepareStatement(query);
+
+                int n = pstm.executeUpdate();
+
+                if (n == 1) {
+                    msg = "Ajout projet effectué";
+                } else {
+                    msg = "Ajout projet non effectué";
+                }
+            } catch (Exception e) {
+                msg = "Erreur lors de l'ajout du projet " + e;
+            } finally {
+                try {
+                    if (pstm != null) {
+                        pstm.close();
+                    }
+                } catch (Exception e) {
+                    System.out.println("erreur de fermeture du preparedStatement " + e);
+                }
+
+            }
+        }
+
+        return msg;
+    }
+
+}
